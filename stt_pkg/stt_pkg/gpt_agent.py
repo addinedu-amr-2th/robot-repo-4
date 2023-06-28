ddbfea5d47bc07de96344cd450ff983d239b5dbc
송승훈 <69943723+Song-Seng-Hun@users.noreply.github.com>
2023-06-29 03:59:19 +0900

    Update gpt_agent.py

--- stt_pkg/stt_pkg/gpt_agent.py
+++ stt_pkg/stt_pkg/gpt_agent.py
@@ -5,7 +5,7 @@ from std_msgs.msg import String
 import re
 
 API_PATH = '/home/ane4/Downloads/api.txt'
-MODEL = 'davinci:ft-personal-2023-06-25-01-27-41'
+MODEL = 'gpt-3.5-turbo'
 USE_FEWSHOT = False
 
 class ChatGPT(Node):
